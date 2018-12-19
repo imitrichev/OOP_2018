42350b9b4c74c0fbf4135be07f7b333a95b1a0a4
zzoff <kopasema@yandex.ru>
2018-12-19 15:50:21 +0300

    Минус машины

--- oop4_2.cpp
+++ oop4_2.cpp
@@ -2,44 +2,6 @@
 #include <string>
 using std::string, std::cout, std::endl;
 
-class Engine
-{
-	int VIN;
-	public:
-	    Engine(){}
-	    Engine(int _v):VIN(_v){}
-		void setVIN(int _v){VIN=_v;}
-	    int getVIN(){return VIN;}
-};
-
-// 1. copy constructor
-class Car
-{
-    double fuel;
-    Engine* engine;
-    public:
-        Car(int _v){engine = new Engine(_v);}
-        Car(const Car& that)
-        {
-            engine = new Engine();
-        	*engine = *(that.engine);
-        }
-        // 2. copy assignment operator
-        Car& operator=(const Car& that)
-        {
-            if (this != &that)
-            {
-                delete engine;
-                engine = new Engine();
-        	    *engine = *(that.engine);
-            }
-            return *this;
-        }
-        ~Car() {delete engine;}
-        void printInfo(){cout<<engine<<" "<<engine->getVIN()<<endl;}
-		void setVIN(int _v){engine->setVIN(_v);}
-};
-
 int main()
 {
   Car* mashinka [2];
